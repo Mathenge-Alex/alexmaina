031778f1b2353a48937a843e02177ec07f3545ca
Mathenge-Alex <arxmaina@gmail.com>
2024-03-07 21:02:24 +0300

    Feat: Add script to navigate carousel section

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,9 @@ document.getElementById("hamburger").addEventListener("click", function () {
 });
 
 // Navbar view:
-window.onscroll = function() {
+window.onscroll = function () {
     const navbar = document.querySelector('.navbar');
-    if(window.pageYOffset > 100) {
+    if (window.pageYOffset > 100) {
         navbar.classList.add('scrolled');
     } else {
         navbar.classList.remove('scrolled');
@@ -14,3 +14,27 @@ window.onscroll = function() {
 };
 
 
+
+
+// Carousel
+let currentSlide = 0;
+const slides = document.querySelectorAll(".carousel__item");
+
+function showSlide(index) {
+    if (index >= slides.length) index = 0;
+    if (index < 0) index = slides.length - 1;
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+    currentSlide = index;
+}
+
+function moveSlide(change) {
+    showSlide(currentSlide + change);
+}
+
+// Initialize the carousel with the first item active
+showSlide(currentSlide);
